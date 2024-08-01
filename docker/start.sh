afd058a85a84d629103944a7755e6886bf388756
Ali <alli.fartout@gmail.com>
2024-08-01 17:35:03 +0330

    fixing Persistent Storage, entrypint and authentication

---
+++ docker/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python manage.py makemigrations
+python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('ali', '', 'Pol@ris01')"
+
+python manage.py runserver 0.0.0.0:8000
