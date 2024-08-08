7a31aaaeda2cc0625c200dd724bb227ca4eecd05
Ali <alli.fartout@gmail.com>
2024-08-08 15:49:31 +0330

    Complete Crawler api

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -5,6 +5,7 @@ import re
 import poster
 from datetime import datetime
 import pytz
+from datetime import date, datetime
 
 
 url = "https://www.waifu.im/search/?included_tags=waifu"
@@ -14,19 +15,19 @@ soup = BeautifulSoup(html_content, 'html.parser')
 script_tags = soup.find_all()
 img_urls = re.findall(r'https://cdn\.waifu\.im/\d+\.(?:jpg|jpeg|png)', str(script_tags))
 
-cwd = os.getcwd()
-save_dir = os.path.join(cwd, "cache_images")
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
+
 
 for img_url in img_urls:
+
     data = {
         "hashed_image_id": img_url,
         "publisher_id": "Crawler",
-        "published_date": None,
+        "published_date": date.today().isoformat(),
         "liked_count": 0,
         "disliked_count": 0
     }
-    poster.post_data(data)
-    print(save_dir)
-    # poster.download_image(img_url, save_path)
+
+    response = poster.post_data(data)
+    if response: 
+        poster.download_image(img_url, os.path.join(os.getcwd(), "cache_images")
+)
--- Crawler/poster.py
+++ Crawler/poster.py
@@ -1,24 +1,42 @@
 import requests
 import json
 import os
+from urllib.parse import urlparse
 
 
+import requests
+import json
 
 def post_data(data):
     api_url = "http://172.21.0.5:8000/api/post/"
+    check_url = f"{api_url}check/{data['hashed_image_id']}/"  # New URL for checking existing data
     headers = {
         "Content-Type": "application/json"
     }
+
+    check_response = requests.get(check_url, headers=headers)
+    
+    if check_response.status_code == 200:
+        print(f"Data with hashed_image_id {data['hashed_image_id']} already exists. Skipping.")
+        return False 
+    
     response = requests.post(api_url, headers=headers, data=json.dumps(data))
     
     if response.status_code == 201:
         print("Data posted successfully")
+        return True
     else:
         print(f"Failed to post data: {response.status_code}")
+        return False
+
 
 
         
 def download_image(url, save_path):
+
+    filename = os.path.basename(urlparse(url).path)
+    save_path = os.path.join(save_path, filename)
+
     response = requests.get(url)
     with open(save_path, 'wb') as file:
         file.write(response.content)
